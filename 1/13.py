225a977c61df7567dbec9dcfa51e6c3ed3e8be8e
ammuiyer <ammu.iyer5@gmail.com>
2023-01-13 10:57:22 -0800

    day 2, starting blind 75 for interview prep

---
+++ 1/13.py
@@ -0,0 +1,17 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ans = []
+        for i in range(0, len(nums)):
+            for j in range(i+1, len(nums)):
+                if(nums[i]+nums[j]==target):
+                    ans.append(i)
+                    ans.append(j)
+                    return ans
+                    
