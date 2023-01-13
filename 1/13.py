763bd85563d3c93eaf987b05fa7fdce46fdd2850
ammuiyer <ammu.iyer5@gmail.com>
2023-01-13 12:57:04 -0800

    __

--- 1/13.py
+++ 1/13.py
@@ -14,4 +14,14 @@ class Solution(object):
                     ans.append(i)
                     ans.append(j)
                     return ans
-                    
+
+#Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(set(nums))!=len(nums)
+            
+            
