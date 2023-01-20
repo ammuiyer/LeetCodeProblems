93c3ad74c2b5a0dcbac26de326adf478e2d506a8
ammuiyer <ammu.iyer5@gmail.com>
2023-01-19 18:18:15 -0800

    reversing a LL

---
+++ 1/19.py
@@ -0,0 +1,23 @@
+#
+# Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        next = None
+        current = head
+        prev = None
+        while(current!=None):
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        return prev
